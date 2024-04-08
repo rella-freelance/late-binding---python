ca54c2ce1817bab5a0dc15ed5d98e51d8fe4fc29
rella <165660981+rella-freelance@users.noreply.github.com>
2024-04-08 19:34:31 +0300

    Add files via upload

---
+++ funlist.py
@@ -0,0 +1,21 @@
+fun_list1 = []
+for i in range(5):
+	def fun1(e):
+		return e+i
+	fun_list1.append(fun1)
+
+fun_list2 = []
+for i in range(5):
+	def fun2(e, iv=i):
+		return e+iv
+	fun_list2.append(fun2)
+
+fun_list3 = [lambda e: e+e for i in range(5)]
+
+fun_list4 = [lambda e, iv=i: e+iv for i in range(5)]
+
+i=55
+print([f(10) for f in fun_list1])
+print([f(10) for f in fun_list2])
+print([f(10) for f in fun_list3])
+print([f(10) for f in fun_list4])
