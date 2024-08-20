7aaa5787776782436b3130100348f5877a74bcb7
rella <165660981+rella-freelance@users.noreply.github.com>
2024-08-20 21:54:21 +0300

    Update funlist.py

--- funlist.py
+++ funlist.py
@@ -9,7 +9,7 @@ for i in range(5):
 	def fun2(e, iv=i):
 		return e+iv
 	fun_list2.append(fun2)
-
+#***
 fun_list3 = [lambda e: e+e for i in range(5)]
 
 fun_list4 = [lambda e, iv=i: e+iv for i in range(5)]
